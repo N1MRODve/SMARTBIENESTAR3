1bc3a8f62c4ebff56a0b44d19147fb3760956c54
N1MRODve <dieterlorenzo@gmail.com>
2025-09-27 10:53:56 +0200

    Verificar y corregir App.vue para renderizado de rutas

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,8 @@
 <template>
-  <router-view v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
-      <component :is="Component" />
-    </transition>
-  </router-view>
+  <router-view />
   <Toast />
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
-import { useAuthStore } from './stores/auth.store';
 import Toast from 'primevue/toast';
-
-const authStore = useAuthStore();
-
-onMounted(async () => {
-  await authStore.initialize();
-});
 </script>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.15s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
